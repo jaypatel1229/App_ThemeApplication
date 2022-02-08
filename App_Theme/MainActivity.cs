cf8f976e6b9374e0e00683702005b396c06be3a6
jay.patel <jay.patel@1R-80>
2022-02-08 12:39:47 +0530

    Add project files.

---
+++ App_Theme/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
+
+namespace App_Theme
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class AppThemeActivity : AppCompatActivity
+    {
+        private TextView _themeTextView;
+        private Button _checkThemeButton;
+      //  private Button _changeThemeButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            UIReferences();
+            UIClickevents();
+        }
+
+        private void UIClickevents()
+        {
+            _checkThemeButton.Click += CheckThemeButton_Click;
+         
+        }
+
+        private void CheckThemeButton_Click(object sender, EventArgs e)
+        {
+            var currentTheme = AppInfo.RequestedTheme;
+            _themeTextView.Text = $"Current theme: {currentTheme}";
+        }
+
+        private void UIReferences()
+        {
+            _themeTextView = FindViewById<TextView>(Resource.Id.ThemView);
+            _checkThemeButton = FindViewById<Button>(Resource.Id.chkThem);
+            
+        }
+    }
+}
